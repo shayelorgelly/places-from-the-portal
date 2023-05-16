0831cbad70bd588a53a2b2a67fa184a10c0cb020
shayelorgelly <shayelorgelly@student.net.nz>
2023-05-17 10:37:05 +1200

    fix ui stuff

--- modules/ui.js
+++ modules/ui.js
@@ -2,11 +2,12 @@ module.exports = {
     getTerminalSize: ()=>{
         return [process.stdout.rows, process.stdout.columns]
     },
-    checkTerminalSize: (rows, columns)=>{
+    checkTerminalSize: (rows, columns)=>{//checks if the terminal size is within the rows/colums
         let currentSize = module.exports.getTerminalSize()
-        let maxSize = [rows, columns]
-        if(mixSize[0] < currentSize[0] || minSize[1] < currentSize){
-            console.warn("no")
+        let minSize = [rows, columns]
+        if(minSize[0] > currentSize[0] || minSize[1] > currentSize){
+            return false// terminal is not big enough to meet rows/columns requirement
         }
+        return true
     }
 }
