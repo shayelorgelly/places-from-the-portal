f31847afb4bd0b31ebb1d5a0cedb530a711838ff
shayelorgelly <shayelorgelly@student.net.nz>
2023-05-17 10:08:36 +1200

    Add color module for people who are using python

--- modules/recolor.py
+++ modules/recolor.py
@@ -1,3 +1,4 @@
+#color module made for the people using python :)
 def colorText(message,rgbarray):
     return f'\x1b[38;2;{";".join(map(str, rgbarray))}m{message}\x1b[0m'
 def colorLog(message,rgbarray):
