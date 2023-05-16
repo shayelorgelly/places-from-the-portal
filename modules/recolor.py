b56a4e927f2712d5ec0e8dfa5f92278899c50e8d
shayelorgelly <shayelorgelly@student.net.nz>
2023-05-17 09:59:35 +1200

    Update recolor.py

---
+++ modules/recolor.py
@@ -0,0 +1,7 @@
+def colorText(message,rgbarray):
+    return f'\x1b[38;2;{";".join(map(str, rgbarray))}m{message}\x1b[0m'
+def colorLog(message,rgbarray):
+    print(f'\x1b[38;2;{";".join(map(str, rgbarray))}m{message}\x1b[0m')
+# import recolor
+# colorLog("hello", [0,255,0]) #green
+# colorText("Hello World!", [255,0,0]) #red
